f5e75b1af6bceb7c29a9b4058a0c6d9acbc24f21
Christian MilNeil <c.neal.milneil@gmail.com>
2017-02-26 15:19:29 -0500

    histogram scripts

--- static/histogram.js
+++ static/histogram.js
@@ -12,7 +12,7 @@ var  width, height, aspect, populationsvg;
 $(function() {
 
   width = $('div#histogram').innerWidth() - margin.left - margin.right,
-  height = 200;
+  height = $('div#histogram').innerHeight() - margin.top - margin.bottom,
   var populationsvg = d3.select('div#histogram').append('svg')
     .attr('width', width + margin.left + margin.right)
     .attr('height',height + margin.top + margin.bottom);
